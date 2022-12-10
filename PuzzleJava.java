142d0d51b5c624fda06229719c484973de15497a
Brian Denmark <testingarea479@gmail.com>
2022-12-09 16:42:35 -0800

    added getTenRolls

--- PuzzleJava.java
+++ PuzzleJava.java
@@ -5,4 +5,11 @@ public class PuzzleJava {
     public int randomRoll(int min, int max) {
         return rand.nextInt((max + 1) - min) + min;
     }
+
+    public int[] getTenRolls(int[] rollArray) {
+        for(int i = 0; i < rollArray.length; i++){
+            rollArray[i] = randomRoll(1,20);
+        }
+        return rollArray;
+    }
 }
--- PuzzleTest.java
+++ PuzzleTest.java
@@ -2,5 +2,12 @@ public class PuzzleTest {
     public static void main(String[] args) {
         PuzzleJava puzzleTime = new PuzzleJava();
         System.out.println(puzzleTime.randomRoll(1,20));
+        int[] tenRolls = new int[10];
+        puzzleTime.getTenRolls(tenRolls);
+        for (int i = 0; i < tenRolls.length; i++) {
+            System.out.println(tenRolls[i]);
+        }
+        String output = String.format("tenRolls length is: %s",tenRolls.length);
+        System.out.println(output);
     }
 }
